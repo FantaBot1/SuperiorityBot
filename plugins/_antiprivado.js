1a32c50759e54cef42f8096f54c454c4b7cbe259
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-27 10:20:32 -0500

    𝗦𝘂𝗽𝗲𝗿𝗶𝗼𝗿𝗶𝘁𝘆 𝗕𝗼𝘁
    𝗦𝘂𝗽𝗲𝗿𝗶𝗼𝗿𝗶𝘁𝘆 𝗕𝗼𝘁

---
+++ plugins/_antiprivado.js
@@ -0,0 +1,15 @@
+// TheMystic-Bot-MD@BrunoSobrino - _antiprivado.js
+
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`*SUPERIORITY BOT*\n\n*SERAS BLOQUEADX SI CREES QUE FUE UN ERROR HABLALE A MI CREADOR*`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+  }
+  return !1;
+}
